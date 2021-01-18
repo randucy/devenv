8e2903206482efee5658def62a69b049d7f272dd
Thijs Boom <twboom@hotmail.nl>
2021-01-18 16:17:57 +0100

    Created init.js

--- devenv/js/init.js
+++ devenv/js/init.js
@@ -1,6 +1,23 @@
 function init() {
   let params = (new URL(document.location)).searchParams;
+  let randucy = params.get('randucy');
   let ducy = params.get('ducy')
+
+  if (randucy !== 'false') {
+    const devEnvRanDucyURL = [
+      "https://randucy.netlify.app/js/randucy.js",
+      "https://randucy.netlify.app/js/functions.js",
+      "https://randucy.netlify.app/randucy/randucy.js",
+      "https://randucy.netlify.app/randucy/randucyData.js"
+    ]
+
+    for (let i = 0; i < devEnvRanDucyURL.length; i++) {
+      let newScript = document.createElement('script')
+      newScript.setAttribute('src',devEnvRanDucyURL[i])
+      document.body.appendChild(newScript)
+    };
+    console.log('RanDucy DevEnv: Initated RanDucy')
+  }
 
   if (ducy == 'true') {
     const devEnvDucyURL = [
