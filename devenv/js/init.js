7e828feb2a7526ae5f9be47320899ffad9155dff
Thijs Boom <twboom@hotmail.nl>
2021-01-18 16:12:11 +0100

    Made DUCY activator

--- devenv/js/init.js
+++ devenv/js/init.js
@@ -1,5 +1,24 @@
 function init() {
+  let params = (new URL(document.location)).searchParams;
+  let ducy = params.get('ducy')
 
+  if (ducy == 'true') {
+    const devEnvDucyURL = [
+      "https://ducy.netlify.app/ducyFunctions.js",
+      "https://ducy.netlify.app/ducyInstall.js",
+      "https://ducy.netlify.app/ducyList.js",
+      "https://ducy.netlify.app/ducyHelp.js",
+      "https://ducy.netlify.app/ducyData.js"
+    ]
+
+    for (let i = 0; i < devEnvDucyURL.length; i++) {
+      let newScript = document.createElement('script')
+      newScript.setAttribute('src',devEnvDucyURL[i])
+      document.body.appendChild(newScript)
+    };
+
+    console.log('RanDucy DevEnv: Initated DUCY')
+  }
 }
 
 init()
